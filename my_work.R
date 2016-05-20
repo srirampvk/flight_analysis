9518082bf621098c58b43e8fdbb5eaf4198c2cba
Sriram Karunamoorthy <skarunamoorthy@mbp-002762.ad.whirl.net>
2016-05-19 19:31:09 -0700

    #this scenario is to make changes here, push it to remote. In the test_flight repo i have un committed changes similar to box scenario. Let us see how it proceeds.

--- my_work.R
+++ my_work.R
@@ -6,6 +6,7 @@ library(lubridate)
 #just doing a test on Rebase as I had a problem on 2016-05-18 when working at Box
 #test again
 #this is to perform changes in my repo to replicate the current scenario at Box - make changes in here, push to remote. then fetch from test_flight repo, rebase.
+#this scenario is to make changes here, push it to remote. In the test_flight repo i have un committed changes similar to box scenario. Let us see how it proceeds.
 setwd("C:/Sriram/Resumes/Full time/The advisory Board/data")
 
 #Browse the pwd. This is actually not needed but just for my personal purpose
